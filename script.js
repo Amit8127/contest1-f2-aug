990c7c8a1643e48d2c76b54c74ed5688cc3fef03
Amit8127 <amitmonu0000@gmail.com>
2023-08-12 15:01:36 +0530

    Submission

--- script.js
+++ script.js
@@ -9,18 +9,47 @@ let arr = [
 
 function PrintDeveloper() {
   //Write your code here , just console.log
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]["profession"] == "developer") {
+      console.log(arr[i]);
+    }
+  }
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newObj = {
+    id: 4,
+    name: "susan",
+    age: 20,
+    profession: "intern"
+  }
+
+  arr.push(newObj);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]["profession"] == "admin") {
+      arr.pop(arr[i]);
+    }
+  }
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+    { id: 1, name: 'john', age: 18, profession: 'developer' },
+    { id: 2, name: 'jack', age: 20, profession: 'developer' },
+    { id: 3, name: 'karen', age: 19, profession: 'admin' },
+  ];
+
+  arr.push(newArr);
+  console.log(arr);
+
 }
 
 // Here is an example of how functions work,
